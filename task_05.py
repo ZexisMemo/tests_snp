85fb3a8c5df0e3ec6a682c90dd072e358ec7ccb8
ZexisMemo <132081934+ZexisMemo@users.noreply.github.com>
2023-05-03 23:58:59 +0300

    Update task_05.py

--- task_05.py
+++ task_05.py
@@ -1,11 +1,8 @@
 import time
 def date_in_future(integer):
     if type(integer) != int:
-        print(time.strftime("%d-%m-%Y %H:%M:%S", time.localtime()))
+        return time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
     else:
-        print(time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time() + 24*3600*integer)))
+        return time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time() + 24*3600*integer))
 
 
-date_in_future([])
-date_in_future(2)
-date_in_future(60)
