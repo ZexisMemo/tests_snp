5b575ac102933494eac67abd5c02d567c27cb7e9
ZexisMemo <132081934+ZexisMemo@users.noreply.github.com>
2023-05-03 23:57:56 +0300

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -5,16 +5,5 @@ def max_odd(array):
             if array[i]%2 != 0 and array[i] > maxoddelem:
                 maxoddelem = int(array[i])
     if maxoddelem == -1:
-        print(None)
-    else:
-        print(maxoddelem)
+        return None
     return maxoddelem
-
-
-max_odd([1, 2, 3, 4, 4])
-max_odd([21.0, 2, 3, 4, 4])
-max_odd(['ololo', 2, 3, 4, [1, 2], None])
-max_odd(['ololo', 'fufufu'])
-max_odd([2, 2, 4])
-
-                
