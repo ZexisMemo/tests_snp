5ba109a81d52b40288bd25720c1d5834bf3d42ee
ZexisMemo <132081934+ZexisMemo@users.noreply.github.com>
2023-05-04 00:01:33 +0300

    Update task_11.py

--- task_11.py
+++ task_11.py
@@ -1,31 +1,25 @@
 class Dessert:
-    def __init__(self, name = "", calories = 0):
-        self._name = name
-        self._calories = calories
+    def __init__(self, name = "", calories = ""):
+        self.name = name
+        self.calories = calories
 
     def set_name(self, name):
-        self._name = name
+        self.name = name
 
     def get_name(self):
-        return self._name
+        return self.name
 
     def set_calories(self, calories):
-        self._calories = calories
+        self.calories = calories
 
     def get_calories(self):
-        return self._calories
+        return self.calories
 
     def is_healthy(self):
-        if self._calories < 200:
-            return True
+        if type(self.calories) == int or type(self.calories) == float:
+            if self.calories < 200:
+                return True
         return False
 
     def is_delicious(self):
         return True
-
-
-
-
-Cake = Dessert("Cake", 500)
-Cake2 = Dessert("Cake", 450)
-Jelly = Dessert("Jelly", 150)
