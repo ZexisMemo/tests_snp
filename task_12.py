2e9f466a4a0a31d1ee2491e3d8aed4eebec7a190
ZexisMemo <132081934+ZexisMemo@users.noreply.github.com>
2023-05-04 00:02:08 +0300

    Update task_12.py

--- task_12.py
+++ task_12.py
@@ -1,40 +1,37 @@
 class Dessert:
-    def __init__(self, name = "", calories = 0, flavor = ""):
-        self._name = name
-        self._calories = calories
-        self._flavor = flavor
+    def __init__(self, name = "", calories = "", flavor = ""):
+        self.name = name
+        self.calories = calories
+        self.flavor = flavor
 
     def set_name(self, name):
-        self._name = name
+        self.name = name
 
     def get_name(self):
-        return self._name
+        return self.name
 
     def set_calories(self, calories):
-        self._calories = calories
+        self.calories = calories
 
     def get_calories(self):
-        return self._calories
+        return self.calories
 
     def is_healthy(self):
-        if self._calories < 200:
-            return True
+        if type(self.calories) == int or type(self.calories) == float:
+            if self.calories < 200:
+                return True
         return False
 
     def is_delicious(self):
-        if self._flavor == "black licorice":
+        if self.flavor == "black licorice":
             return False
         return True
 
 class JellyBean(Dessert):
     
     def set_flavor(self, flavor):
-        self._flavor = flavor
+        self.flavor = flavor
 
     def get_flavor(self):
-        return self._flavor
+        return self.flavor
 
-
-Cake = Dessert("Cake", 500)
-Cake2 = Dessert("Cake", 450)
-Jelly = JellyBean("Jelly", 150)
