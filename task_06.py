a3ea6900ccba8dd469e5113c17651e04958da760
ZexisMemo <132081934+ZexisMemo@users.noreply.github.com>
2023-05-19 16:07:19 +0300

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -1,16 +1,16 @@
-class MyError(Exception):
+class WrongNumberOfPlayersError(Exception):
+    pass
+
+class NoSuchStrategyError(Exception):
     pass
 
 def rps_game_winner(list):
-    try:
-        if len(list) >= 3:
-            raise MyError("WrongNumberOfPlayersError")
-        elif list[0][1] != 'P' and list[0][1] != 'R' and list[0][1] != 'S':
-            raise MyError("NoSuchStrategyError")
-        elif list[1][1] != 'P' and list[1][1] != 'R' and list[1][1] != 'S':
-            raise MyError("NoSuchStrategyError")
-    except MyError as err:
-        print(err.args[0])
+    if len(list) != 2:
+        raise WrongNumberOfPlayersError()
+    elif list[0][1] != 'P' and list[0][1] != 'R' and list[0][1] != 'S':
+        raise NoSuchStrategyError()
+    elif list[1][1] != 'P' and list[1][1] != 'R' and list[1][1] != 'S':
+        raise NoSuchStrategyError()
     else:
         a = ord(list[0][1]) - ord(list[1][1])
         if (a <= 0 or a == 3) and a != -3:
@@ -23,4 +23,5 @@ def rps_game_winner(list):
         else:
             string = list[1][0] + " " + list[1][1]
         return string
-    return
+
+
